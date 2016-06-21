71a7945fb7a0c4fcde3b7a4b635b419fc879f18c
Grzegorz Szpak <gszpak@student.uw.edu.pl>
2016-06-21 20:52:33 +0200

    Fixes in ccp

--- ccp.c
+++ ccp.c
@@ -6,21 +6,35 @@
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#define SYS_cow_cp 359
+
 int main(int argc, char *argv[])
 {
-    long src_fd, dst_fd;
+    long src_fd, dst_fd, res;
+
+    if (argc != 3) {
+        printf("Usage: %s <source_file> <destination_file>\n", argv[0]);
+        exit(1);
+    }
 
     src_fd = open(argv[1], O_RDONLY);
     if (src_fd < 0) {
         perror("Error while opening source file\n");
+        exit(1);
     }
-    dst_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+    dst_fd = open(argv[2], O_RDWR | O_CREAT);
     if (src_fd < 0) {
-        perror("Error while opening source file\n");
+        perror("Error while opening destination file file\n");
+        close(src_fd);
+        exit(1);
     }
-    long syscall_res = syscall(359, src_fd, dst_fd);
-    //printf("System call sys_hello returned %ld\n", syscall_res);
+
+    res = syscall(359, src_fd, dst_fd) < 0);
     close(src_fd);
     close(dst_fd);
-    return 0;
+    if (res < 0) {
+        perror("Error in syscall\n");
+    }
+    return res;
 }
